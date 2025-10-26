99926a47e83535f76cea790f24f9148386da9753
vitor.oliveira <vitor.oliveira@agenciamolla.com.br>
2025-10-26 18:27:40 -0300

    chore: removendo footer sources

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -57,12 +57,12 @@ export default async function Footer() {
           </p>
           <hr className="mx-4 hidden h-4 w-[1px] border-l border-neutral-400 md:inline-block" />
           <p>
-            <a href="https://github.com/vercel/commerce">View the source</a>
+            {/* <a href="https://github.com/vercel/commerce">View the source</a> */}
           </p>
           <p className="md:ml-auto">
-            <a href="https://vercel.com" className="text-black dark:text-white">
+            {/* <a href="https://vercel.com" className="text-black dark:text-white">
               Created by ▲ Vercel
-            </a>
+            </a> */}
           </p>
         </div>
       </div>
