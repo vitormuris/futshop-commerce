751db1dee99b7b801a2b667fb9e79ad15e43b4bc
vitor.oliveira <vitor.oliveira@agenciamolla.com.br>
2025-10-26 18:26:08 -0300

    chore: removendo welcome toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,11 @@
 import { CartProvider } from 'components/cart/cart-context';
 import { Navbar } from 'components/layout/navbar';
-import { WelcomeToast } from 'components/welcome-toast';
 import { GeistSans } from 'geist/font/sans';
 import { getCart } from 'lib/shopify';
+import { baseUrl } from 'lib/utils';
 import { ReactNode } from 'react';
 import { Toaster } from 'sonner';
 import './globals.css';
-import { baseUrl } from 'lib/utils';
 
 const { SITE_NAME } = process.env;
 
@@ -38,7 +37,7 @@ export default async function RootLayout({
           <main>
             {children}
             <Toaster closeButton />
-            <WelcomeToast />
+            {/* <WelcomeToast /> */}
           </main>
         </CartProvider>
       </body>
